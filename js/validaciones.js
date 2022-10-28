61e7218ae1edcb2ad457d2a79310d21d94a34ded
Crisppu <cristobalyat69@hotmail.com>
2022-10-27 18:05:57 -0600

    funcion de objeto de mensajes

--- js/validaciones.js
+++ js/validaciones.js
@@ -5,24 +5,58 @@
 //     validarNacimiento(evento.target);
 // });
 
-export function valida(input){ // exportamos la funcion
+export function valida(input) { // exportamos la funcion
     const tipoDeInput = input.dataset.tipo; //dataset decimos que nos muestre todos los data dentro de nuestro html y me muestre solo el que tenga por nombre tipo
-    console.log(tipoDeInput)
-    console.log(validadores[tipoDeInput])
-    if(validadores[tipoDeInput]){ // le decimos si dentro de validadores existe e tipo de input
+   //console.log(tipoDeInput)
+    //console.log(validadores[tipoDeInput])
+    if (validadores[tipoDeInput]) { // le decimos si dentro de validadores existe e tipo de input
         validadores[tipoDeInput](input); //si existe le decimos
     }
+
+    //console.log(input.parentElement)
+    //introducir $0 y $0.validity en la consola del navegador
+    if (input.validity.valid) { //verificamos si e valid es falso o verdadero
+        input.parentElement.classList.remove("input-container--invalid");
+    } else {
+        input.parentElement.classList.add("input-container--invalid")
+    }
 }
+//creamos un objeto para mostrar que mensajes colocar
+//introducir $0 y $0.validity en la consola del navegador
+const mensajesDeError = {
+    nombre: {
+         //introducir $0 y $0.validity en la consola del navegador
+        valueMissing: "este campo no debe de estar vacio" // valueMissing en caso de que el campo este vacio
+    },
+    email: {
+        //introducir $0 y $0.validity en la consola del navegador
+        valueMissing: "este campo no debe de estar vacio",
+        typeMismatch: "el correo no es valido"  //este tipo el typeMismatch hace referencia a que es correo electronico
+    },
+    password:{
+        //introducir $0 y $0.validity en la consola del navegador
+        valueMissing: "este campo no debe de estar vacio",
+        //traemos el texto que estaba dentro del titulo en el html input name="password"
+        patternMismatch:"al menos 5 caracteres, maximo 12, debe contener una letra minuscula, una letra MAYUSCULA, un numero y no debe tener caracteres especiales"
+    },
+    //input de fecha
+    nacimiento:{
+         //introducir $0 y $0.validity en la consola del navegador
+        valueMissing: "este campo no debe de estar vacio",
+        customError:"Debes de tener al menos 18 años"
+    },
+
+};
 
 
 /*nos sirve para verificar si existe el input con el nombre de data-tipo="nacimiento" esto es un objeto*/
 const validadores = { //este objeto es para validar para que coincidan con el tipo y el nombre que le colocamos en el html "nacimiento"
     /* arrow function Si el cuerpo tiene una declaración o expresión única, puede escribir la función de flecha como */
-   nacimiento: input => validarNacimiento(input), //funcion anonima,
+    nacimiento: input => validarNacimiento(input), //funcion anonima,
 
-//    nacimiento: function (input){
-//      validarNacimiento(input)
-//      }
+    //    nacimiento: function (input){
+    //      validarNacimiento(input)
+    //      }
 };
 
 function validarNacimiento(input) {
@@ -32,13 +66,13 @@ function validarNacimiento(input) {
     //console.log(fechaCliente);
     //mayorDeEdad(fechaCliente);
     let mensaje = "";
-//opcion2
-        if (!mayorDeEdad(fechaCliente)) {// true : sino eres verdadero lo que hicimos aqui fue negarlo !
-            mensaje = "debes de tener al menos 18 años";
-        }
-//opcion1
-// if(mayorDeEdad(fechaCliente) === false){
-//     mensaje = "debes de tener al menos 18 años";}
+    //opcion2
+    if (!mayorDeEdad(fechaCliente)) {// true : sino eres verdadero lo que hicimos aqui fue negarlo !
+        mensaje = "debes de tener al menos 18 años";
+    }
+    //opcion1
+    // if(mayorDeEdad(fechaCliente) === false){
+    //     mensaje = "debes de tener al menos 18 años";}
 
     //console.log(mayorDeEdad(fechaCliente));
     input.setCustomValidity(mensaje); //método de la HTMLObjectElementinterfaz establece un mensaje de validez personalizado para el elemento
